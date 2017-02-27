d5d32faf4d3555a125bc65f75a34b800ed509bd4
Richard Dotchin <richard.dotchin@gmail.com>
2017-02-26 21:18:53 -0500

    scrape working

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,4 +4,9 @@ exports.create = function(req, res){
 	var article = new News({
 
 	});
+
+	entry.save();
+
+	//redirect to home page
+	res.redirect('/');
 };
--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,22 +1,8 @@
 const cheerio = require('cheerio');
-/*==========MONGOOSE=========================*/
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongojs = require('mongojs');
+const request = require('request');
 
 
-mongoose.connect('mongodb://localhost/nytdb');
-
-//create a schema
-var newsSchema = Schema({
-	headline: String,
-	summary: String,
-	link: String,
-	img: String,
-	notes: Array
-});
-
-//create a model 
-var News = mongoose.model('News', newsSchema);
 
 /*===============================ROUTES======================================*/
 module.exports= function(app){
@@ -29,8 +15,27 @@ module.exports= function(app){
 	});
 
 	app.get('/scrape', function(req, res){
+        //make a request to the NYT site to grab articles
+        request('https://www.nytimes.com/section/world/americas', function(err, response, html){
+            //Load the html body from request into cheerio
+            const $ = cheerio.load(html);
 
-		console.log(res);
+            //For each element with a "story" class
+            $('.story-body').each(function(i, element){
+                // Save the text of the title
+                const title = $(this).find("h2").text();
+                // Save the link for the story
+                const link = $(this).children("a").attr("href");
+                // Save the text summary for the story
+                const summary = $(this).find("p").text();
+
+               console.log(title);
+                console.log(link);
+                console.log(summary);
+            })
+
+        });
+
 		res.redirect('/');
 	});
 
