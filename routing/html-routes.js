e40f434bec5981fff43476d61531924240b10f1a
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-01 23:26:01 -0500

    app working

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const Note = require("./../models/noteModel.js");
-const News = require("./../models/newsmodel.js");
-const scraper = require('./../controllers/controller.js')
+const Note = require("./../models/noteModel.js"); //require the Note model
+const News = require("./../models/newsmodel.js"); //require the News model
+const scraper = require('./../controllers/controller.js'); //require the scrape function
+
 // Database configuration with mongoose
 mongoose.connect('mongodb://localhost/nytdb');
 const db = mongoose.connection;
@@ -17,7 +18,9 @@ db.once("open", function() {
 });
 
 /*===============================ROUTES======================================*/
+//export, will be used in server.js
 module.exports= function(app) {
+    /*HOME PAGE*/
     app.get('/', function (req, res) {
         //find all the news articles in the mongoDB
         News.find({}, function (error, doc) {
@@ -33,26 +36,29 @@ module.exports= function(app) {
         });
     });
 
+    /*when the scrape button is pressed it will run teh scrape function from controllers/controller.js*/
     app.get('/scrape', function (req, res) {
             //call the scrape function from controller.js to initiate the scrape
             scraper.scrape(function(){
-                /*cb from scrape function and redirect back to the homepage to display
-                the scraped data*/
+                /*callback from scrape function and redirect back to the homepage to display
+                the scraped articles*/
                 res.redirect('/');
             });
     });
-    // Set the new article saved to true
+
+    // Save the article by updating the articles saved to 1(true);
     app.get('/save/:id?', function (req, res) {
-        // Set the _id of the article the user would like to save to a variable
+        // Set the _id retrieved from the req.params.id of the article the user would like to save to a variable
         var id = req.params.id;
         // Find the news article by id
         News.findById(id, function (err, news) {
             if (err) return handleError(err);
-
+            //set saved to 1(true)
             news.saved = 1;
+            //save the update in mongoDB
             news.save(function (err, updatedNews) {
                 if (err) return handleError(err);
-                res.redirect('/');
+                //no redirect since it is only updating data and not affecting the view
             })
 
         })
@@ -73,32 +79,26 @@ module.exports= function(app) {
     });
     // Delete News article from teh saved articles page
     app.get('/delete/:id?', function (req, res) {
-        // set the _id of the article the user would like to delete from saved to a variable
-        var id = req.params.id;
+        var id = req.params.id; // set the _id of the article the user would like to delete from saved to a variable
         // Find the news article by id
         News.findById(id, function (err, news) {
-            //set saved to 0(false) so it will be removed from the saved page
-            news.saved = 0;
+            news.saved = 0; //set saved to 0(false) so it will be removed from the saved page
+
             // save the updated changes to the article
             news.save(function (err, updatedNews) {
-                if (err) return handleError(err);
-                //redirect back to the saved page
-                res.redirect('/saved');
+                if (err) return handleError(err); //if err
+                res.redirect('/saved'); //redirect back to the saved page as the updated data will effect the view
             })
         })
     });
 
+    //retrieve the notes attached to saved articles to be displayed in the notes modal
     app.get('/notes/:id', function (req, res) {
         //Query to find the matching id to the passed in it
         News.findOne({_id: req.params.id})
-        //Populate all of the notes associated with it
-            .populate("notes")
-            //execute the query
-            .exec(function (error, doc) {
-                // Log any errors
-                if (error) {
-                    console.log(error);
-                }
+            .populate("notes") //Populate all of the notes associated with it
+            .exec(function (error, doc) { //execute the query
+                if (error) console.log(error);
                 // Otherwise, send the doc to the browser as a json object
                 else {
                     console.log(doc);
@@ -108,17 +108,18 @@ module.exports= function(app) {
     });
 
     app.post('/notes/:id', function (req, res) {
+        //create a new note with req.body
         var newNote = new Note(req.body);
-        console.log(req.body);
-        //save newNote to teh db
+        //save newNote to the db
         newNote.save(function (err, doc) {
             // Log any errors
             console.log('DOC ID');
             console.log(doc.id);
             /*if (error) console.log(error);*/
+            //find and update the note
             News.findOneAndUpdate(
-                {_id: req.params.id},
-                {$push: {notes: doc.id}},
+                {_id: req.params.id}, // find the _id by req.params.id
+                {$push: {notes: doc.id}}, //push to the notes array
                 {new: true},
                 function(err, newdoc){
                 if(err) console.log(err);
@@ -129,4 +130,4 @@ module.exports= function(app) {
     });
 
 
-};
+}; //end of module.exports
