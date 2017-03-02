298c2398668b686805e0c5dc59fe99ff19d136e1
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-01 23:53:58 -0500

    heroku

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -8,7 +8,7 @@ var Promise = require('bluebird');
 mongoose.Promise = Promise;
 
 // Database configuration with mongoose
-mongoose.connect('mongodb://localhost/nytdb' || "mongodb://heroku_wxgbzx60@ds113630.mlab.com:13630/heroku_wxgbzx60");
+mongoose.connect('mongodb://localhost/nytdb' || "mongodb://heroku_wxgbzx60:i3mdn92ih8nmleccr2q7ohj5i2@ds113630.mlab.com:13630/heroku_wxgbzx60");
 const db = mongoose.connection;
 
 // Show any mongoose errors
